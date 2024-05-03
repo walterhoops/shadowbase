35ba280adb90c467ec38a44e6f9ac165f5fa28aa
walt <120518624+walterhoops@users.noreply.github.com>
2024-05-02 23:15:17 -0700

    fix select button in Users (#24)
    Co-authored-by: Thanya <88644101+Bthyw@users.noreply.github.com>

---
+++ shadowbase/Models/SchoolViewModels/UserIndexData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace shadowbase.Models.SchoolViewModels
+{
+    public class UserIndexData
+    {
+        public IEnumerable<User> Users { get; set; }
+        public IEnumerable<Bid> Bids { get; set; }
+    }
+}
